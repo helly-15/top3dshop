0f9dd75848a3d63a187737afdf0dd4426ef69aba
Lenocka <lvutina.en@gmail.com>
2021-01-27 21:34:17 +0300

    feat:add contacts section

--- src/header/contacts/Contacts.jsx
+++ src/header/contacts/Contacts.jsx
@@ -1,5 +1,7 @@
 import './Contacts.scss'
-
+import email from '../../assets/email.svg';
+import skype from '../../assets/skype.svg';
+import phone from '../../assets/phone.svg'
 function Contacts(){
 
     return(
@@ -14,12 +16,28 @@ function Contacts(){
                 <a href="tel:+78126135838" className='contacts-phone'>+7 (812) 613-58-38</a>
                 <p className='contacts-time'>Пн–пт <time>10:00–19:00</time>, сб-вс <time>10:00–17:00</time></p>
             </address>
-            <a href = "mailto: abc@example.com">Напишите нам</a>
-            <a href = "mailto: abc@example.com">Свяжитесь с директором</a>
             <nav>
-                <ul>
-                    <li>
-
+                <ul className='connect'>
+                    <li className='connect-write'>
+                        <a href = "mailto: abc@example.com" >Напишите нам</a>
+                    </li>
+                    <li className='connect-director'>
+                        <a href = "mailto: abc@example.com" >Свяжитесь с директором</a>
+                    </li>
+                    <li className='connect-email'>
+                        <a href = "mailto: abc@example.com">
+                            <img src ={email} alt ='write email'/>
+                        </a>
+                    </li>
+                    <li className='connect-skype'>
+                        <a href="skype:********?call">
+                            <img src ={skype} alt ='call Skype'/>
+                        </a>
+                    </li>
+                    <li className='connect-phone'>
+                        <a href="tel:+78126135838">
+                            <img src ={phone} alt ='call Skype'/>
+                        </a>
                     </li>
                 </ul>
             </nav>
