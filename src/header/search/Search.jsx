5e2d36792869c463da22c1314ad72229a7c4a223
Lenocka <lvutina.en@gmail.com>
2021-01-29 12:26:17 +0300

    feat:finish project

--- src/header/search/Search.jsx
+++ src/header/search/Search.jsx
@@ -3,10 +3,13 @@ import logo from '../../assets/logo.svg';
 import yandex from '../../assets/yandex.png';
 import enter from '../../assets/enter.svg';
 import basket from '../../assets/basket.svg';
+import glass from '../../assets/magnifying-glass.svg';
+import logoline from '../../assets/logo_line.png';
 function Search(){
 
     return(
         <div className='search'>
+            <img src={logoline} alt={'site logo'} className='company-logo__line'/>
             <img src={logo} alt={'site logo'} className='company-logo'/>
             <p className='company-logo__text' > Ваш эксперт<br/>
                 на рынке <span>3D</span>-техники</p>
@@ -16,7 +19,12 @@ function Search(){
 
             <form action="#"  className='search-field'>
                 <input className='search-field__place' type="text" placeholder="Введите запрос, например, Formlabs Form 2" name="search"/>
-                <button className='search-field__button' type="submit">Найти</button>
+
+                <button className='search-field__button' type="submit">
+                    <span className='search-field__button_text'> Найти </span>
+                    <img src ={glass} alt ='search' className='search-field__button_glass'/>
+
+                </button>
             </form>
             <div className='auth'>
                 <img className='auth-pic' src ={enter} alt={'enter button'}/>
